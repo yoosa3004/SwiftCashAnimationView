1428629631993361cc10ac1b8f9bae93a0121af3
Hyunndy <yoosa3004@deali.net>
2020-11-24 14:35:37 +0900

    View init

--- SwiftCashAnimationView/Classes/SwiftCashAnimationView.swift
+++ SwiftCashAnimationView/Classes/SwiftCashAnimationView.swift
@@ -7,8 +7,19 @@
 
 import UIKit
 
-open class SwiftCashAnimationView: NSObject {
+public class SwiftCashAnimationView: UIView {
+
     public func DLog(with str: String) {
         print("\(str) from cocoapods")
+    }
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        
+        DLog(with: "init success")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
